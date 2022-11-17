d768ce3090d55c8cd6358b11090b046677e953d7
Manuel Lopez <mao.pelo.lago@gmail.com>
2022-11-17 20:53:15 +0100

    mejora de funcion de entero a romano

--- main.py
+++ main.py
@@ -15,73 +15,39 @@ f)4.5 -> RomanNumberError("Debe ser un entero")
 class RomanNumberError( Exception ):
     pass
 
-diccionario = {
-    'M':1000,
-    'D':500,
-    'C':100,
-    'L':50,
-    'X':10,
-    'V':5,
-    'I':1
-milesima = {
-    1000:'M',2000:'MM',3000:'MMM'
-}
+dic_entero_a_romano={
 
-unidades={
-    'I':1,'II':2,'III':3,
-    'IV':4,'V':5,'VI':6,
-    'VII':7,'VIII':8,'IX':9
     1:'I',2:'II',3:'III',
     4:'IV',5:'V',6:'VI',
-    7:'VII',8:'VIII',9:'IX'
-}
-
-decenas={
-    'X':10,'XX':20,'XXX':30,
-    'XL':40,'L':50,'LX':60,
-    'LXX':70,'LXXX':80,'XC':90
+    7:'VII',8:'VIII',9:'IX',
     10:'X',20:'XX',30:'XXX',
     40:'XL',50:'L',60:'LX',
-    70:'LXX',80:'LXXX',90:'XC'
-}
-
-centenas={
-    'C':100,'CC':200,'CCC':300,
-    'CD':400,'D':500,'DC':600,
-    'DCC':700,'DCCC':800,'CM':900  
+    70:'LXX',80:'LXXX',90:'XC',
     100:'C',200:'CC',300:'CCC',
     400:'CD',500:'D',600:'DC',
-    700:'DCC',800:'DCCC',900:'CM'  
+    700:'DCC',800:'DCCC',900:'CM',  
+    1000:'M',2000:'MM',3000:'MMM'
 }
 
 
 #meta cumplir con esta funcion 
-def entero_a_romano(num):
 
-@@ -58,10 +51,24 @@ def entero_a_romano(num):
-        lista[i] = lista[i]+"0"*((len(lista)-1 )-i)
-    print(lista)
+def entero_a_romano(num:int)->str:
 
-    return lista
+    num = "{:0>4d}".format(num)
+    lista = list(num) 
+    longitud = len(lista)
     numero_romano=""
-    for i in range(len(lista)):
 
-        if i == 0:
-            if milesima.get(int(lista[i])) != None:
-                numero_romano += milesima.get(int(lista[i]) )
+    for i in range(longitud):
 
-        elif i == 1:
-            numero_romano += centenas.get( int(lista[i]) ) 
-        elif i == 2:
-            numero_romano += decenas.get( int(lista[i]) ) 
-        elif i == 3:
-            numero_romano += unidades.get( int(lista[i]) )    
+        longitud -= 1
+        lista[i] += "0"*longitud
+        numero_romano += dic_entero_a_romano.get(int(lista[i]),'' )
 
+     
     return numero_romano
 
 
-
-
 print("funcion en accion",entero_a_romano(336))
-print("funcion en accion",entero_a_romano(1998))
 
